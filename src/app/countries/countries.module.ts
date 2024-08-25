c6e2fe5bc7c8b98b480fdd2a341feec318fd70de
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-25 10:11:00 +0200

    🚧 form

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { CountriesRoutingModule } from './countries-routing.module';
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 
 @NgModule({
   declarations: [SelectorPageComponent],
-  imports: [CommonModule, CountriesRoutingModule],
+  imports: [CommonModule, CountriesRoutingModule, ReactiveFormsModule],
 })
 export class CountriesModule {}
--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SelectorPageComponent {
+  private fb = inject(FormBuilder);
 
+  public myForm: FormGroup = this.fb.group({
+    region: ['', [Validators.required]],
+    country: ['', [Validators.required]],
+    borders: ['', [Validators.required]],
+  });
 }
