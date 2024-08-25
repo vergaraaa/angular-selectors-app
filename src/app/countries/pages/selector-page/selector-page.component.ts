470c429321b6149a4fa4483983acfc6df6b6768f
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-25 10:25:55 +0200

    ✨ countries service

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interfaces';
+
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
@@ -8,10 +11,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SelectorPageComponent {
   private fb = inject(FormBuilder);
+  private countriesService = inject(CountriesService);
 
   public myForm: FormGroup = this.fb.group({
     region: ['', [Validators.required]],
     country: ['', [Validators.required]],
     borders: ['', [Validators.required]],
   });
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
 }
